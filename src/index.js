1327b797beac3cc45743a7285d4820a7d8919944
Artem Deynega <artemdeynega892@gmail.com>
2021-08-30 23:43:11 +0300

    Компоненты-функции

--- src/index.js
+++ src/index.js
@@ -55,9 +55,29 @@ const link = React.createElement(
 
 /////////////////////////// Правило общего родителя
 
-const post = (
-  <>
-    <h2>Post Header</h2>
-    <p>Post text</p>
-  </>
+// const post = (
+//   <>
+//     <h2>Post Header</h2>
+//     <p>Post text</p>
+//   </>
+// );
+
+//////////////////////Компоненты-функции
+// const MyFunctionalComponent = (props) => <div>Functional Component</div>;
+
+const Product = (cart) => (
+  <div>
+    <img
+      src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+      alt="Tacos With Lime"
+      width="640"
+    />
+    <h2>Tacos With Lime</h2>
+    <p>Price: 10.99$</p>
+    <button type="button">Add to cart</button>
+  </div>
 );
+// В разметке компонент записывается как JSX-тег
+ReactDOM.render(<Product />, document.getElementById("root"));
+// Это аналогично
+ReactDOM.render(React.createElement(Product), document.getElementById("root"));
