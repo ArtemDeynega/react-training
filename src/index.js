5deabffc1270a0902e73a2121a94b42fd7116684
Artem Deynega <artemdeynega892@gmail.com>
2021-08-30 22:32:36 +0300

    react вступление и  render

--- src/index.js
+++ src/index.js
@@ -1,42 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+const link = React.createElement(
+  "a",
+  {
+    href: "https://reactjs.org/",
+    target: "_blank",
+    rel: "noreferrer noopener",
+  },
+  "https://ru.reactjs.org/"
+);
 
-const link = React.createElement('a', {
-  href: 'https://reactjs.org/',
-  target: '_blank',
-  rel: 'noreferrer noopener'
-}, 'https://ru.reactjs.org/')
-
-
-const image = React.createElement('img', {
-   src:
-    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
-  alt: 'Tacos With Lime',
+const image = React.createElement("img", {
+  src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+  alt: "Tacos With Lime",
   width: 640,
-})
-const title = React.createElement('h2', null, 'Tacos With Lime');
-const price = React.createElement('p', null, 'Price: 10.99$');
-const button = React.createElement('button', { type: 'button' }, 'Add to cart');
+});
+const title = React.createElement("h2", null, "Tacos With Lime");
+const price = React.createElement("p", null, "Price: 10.99$");
+const button = React.createElement("button", { type: "button" }, "Add to cart");
 
-const product = React.createElement('div', null, image, title, price, button);
+const product = React.createElement("div", null, image, title, price, button);
 
-const productWithChildrenInProps = React.createElement('div', {
-  children: [image, title, price, button]
-})
+/*
+ * Для передачи детей также используется свойство children параметра props.
+ * Обратите внимание на то, что свойство children это массив.
+ */
+const productWithChildrenInProps = React.createElement("div", {
+  children: [image, title, price, button],
+});
 
-ReactDOM.render(product, document.getElementById('root'))
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render(product, document.getElementById("root"));
