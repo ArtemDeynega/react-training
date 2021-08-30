ce6e71df6ce349e986cf666907e1eae60b73adee
Artem Deynega <artemdeynega892@gmail.com>
2021-08-30 23:18:56 +0300

    JSX

--- src/index.js
+++ src/index.js
@@ -14,23 +14,41 @@ const link = React.createElement(
   "https://ru.reactjs.org/"
 );
 
-const image = React.createElement("img", {
-  src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
-  alt: "Tacos With Lime",
-  width: 640,
-});
-const title = React.createElement("h2", null, "Tacos With Lime");
-const price = React.createElement("p", null, "Price: 10.99$");
-const button = React.createElement("button", { type: "button" }, "Add to cart");
+// const image = React.createElement("img", {
+//   src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+//   alt: "Tacos With Lime",
+//   width: 640,
+// });
+// const title = React.createElement("h2", null, "Tacos With Lime");
+// const price = React.createElement("p", null, "Price: 10.99$");
+// const button = React.createElement("button", { type: "button" }, "Add to cart");
 
-const product = React.createElement("div", null, image, title, price, button);
+// const product = React.createElement("div", null, image, title, price, button);
 
-/*
- * Для передачи детей также используется свойство children параметра props.
- * Обратите внимание на то, что свойство children это массив.
- */
-const productWithChildrenInProps = React.createElement("div", {
-  children: [image, title, price, button],
-});
+// /*
+//  * Для передачи детей также используется свойство children параметра props.
+//  * Обратите внимание на то, что свойство children это массив.
+//  */
+// const productWithChildrenInProps = React.createElement("div", {
+//   children: [image, title, price, button],
+// });
+
+// ReactDOM.render(product, document.getElementById("root"));
+
+////////////////JSX
+
+const imageUrl =
+  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+
+const price = 10.99;
+
+const product = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="640" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {price}$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
 
 ReactDOM.render(product, document.getElementById("root"));
