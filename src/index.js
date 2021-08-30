536b5241460abc51209a056c433ad2f6409fd704
Artem Deynega <artemdeynega892@gmail.com>
2021-08-30 23:20:51 +0300

    Правило общего родителя

--- src/index.js
+++ src/index.js
@@ -37,18 +37,27 @@ const link = React.createElement(
 
 ////////////////JSX
 
-const imageUrl =
-  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+// const imageUrl =
+//   "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
 
-const price = 10.99;
+// const price = 10.99;
 
-const product = (
-  <div>
-    <img src={imageUrl} alt="Tacos With Lime" width="640" />
-    <h2>Tacos With Lime</h2>
-    <p>Price: {price}$</p>
-    <button type="button">Add to cart</button>
-  </div>
+// const product = (
+//   <div>
+//     <img src={imageUrl} alt="Tacos With Lime" width="640" />
+//     <h2>Tacos With Lime</h2>
+//     <p>Price: {price}$</p>
+//     <button type="button">Add to cart</button>
+//   </div>
+// );
+
+// ReactDOM.render(product, document.getElementById("root"));
+
+/////////////////////////// Правило общего родителя
+
+const post = (
+  <>
+    <h2>Post Header</h2>
+    <p>Post text</p>
+  </>
 );
-
-ReactDOM.render(product, document.getElementById("root"));
